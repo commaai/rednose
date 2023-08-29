e32658ed9164d6e8dde882c05c5ece9707acde82
Justin Newberry <justin@comma.ai>
2023-08-29 15:51:37 -0700

    remain compatible with other submodules (#35)
    * Revert "propogate ruff (#33)"
    
    This reverts commit 75f545a5e1386b066b86887edecf3272357afc90.
    
    * readd config
    
    * fix ruff
    
    * revert that

--- rednose/helpers/kalmanfilter.py
+++ rednose/helpers/kalmanfilter.py
@@ -10,19 +10,20 @@ class KalmanFilter:
   Q = np.zeros((0, 0))
   obs_noise: Dict[int, Any] = {}
 
-  filter_func = None # Should be initialized when initializating a KalmanFilter implementation
+  # Should be initialized when initializating a KalmanFilter implementation
+  filter = None # noqa: A003
 
   @property
   def x(self):
-    return self.filter_func.state()
+    return self.filter.state()
 
   @property
   def t(self):
-    return self.filter_func.get_filter_time()
+    return self.filter.get_filter_time()
 
   @property
   def P(self):
-    return self.filter_func.covs()
+    return self.filter.covs()
 
   def init_state(self, state, covs_diag=None, covs=None, filter_time=None):
     if covs_diag is not None:
@@ -30,8 +31,8 @@ class KalmanFilter:
     elif covs is not None:
       P = covs
     else:
-      P = self.filter_func.covs()
-    self.filter_func.init_state(state, P, filter_time)
+      P = self.filter.covs()
+    self.filter.init_state(state, P, filter_time)
 
   def get_R(self, kind, n):
     obs_noise = self.obs_noise[kind]
@@ -48,4 +49,4 @@ class KalmanFilter:
     if R is None:
       R = self.get_R(kind, len(data))
 
-    self.filter_func.predict_and_update_batch(t, kind, data, R)
+    self.filter.predict_and_update_batch(t, kind, data, R)
