73c01ebfcca69f6cd1e0dfec91fba8046d13b874
Kacper Rączy <gfw.kra@gmail.com>
2023-11-17 14:47:51 -0800

    RednoseCompileFilter tool

---
+++ site_scons/site_tools/rednose_filter.py
@@ -0,0 +1,46 @@
+from SCons.Script import Dir, File, Glob
+
+
+def compile_single_filter(env, target, filter_gen_script, output_dir, extra_gen_artifacts, script_deps):
+  generated_src_files = [File(f) for f in [f'{output_dir}/{target}.cpp', f'{output_dir}/{target}.h']]
+  extra_generated_files = [File(f'{output_dir}/{x}') for x in extra_gen_artifacts]
+  generator_file = File(filter_gen_script)
+
+  env.Command(generated_src_files + extra_generated_files,
+              [generator_file] + script_deps, f"{generator_file.get_abspath()} {target} {Dir(output_dir).get_abspath()}")
+
+  generated_cc_file = File(generated_src_files[:1])
+
+  return generated_cc_file
+
+
+class BaseRednoseCompileMethod:
+  def __init__(self, base_py_deps, base_cc_deps):
+    self.base_py_deps = base_py_deps
+    self.base_cc_deps = base_cc_deps
+
+
+class CompileFilterMethod(BaseRednoseCompileMethod):
+  def __call__(self, env, target, filter_gen_script, output_dir, extra_gen_artifacts=[], gen_script_deps=[]):
+    objects = compile_single_filter(env, target, filter_gen_script, output_dir, extra_gen_artifacts, self.base_py_deps + gen_script_deps)
+    lib_target = env.SharedLibrary(f'{output_dir}/{target}', [self.base_cc_deps, objects])
+
+    return lib_target
+
+
+def generate(env):
+  rednose_dir = env["REDNOSE_ROOT"]
+
+  templates = Glob(f"{rednose_dir}/rednose/templates/*")
+  sympy_helpers = File(f"{rednose_dir}/rednose/helpers/sympy_helpers.py")
+  ekf_sym = File(f"{rednose_dir}/rednose/helpers/ekf_sym.py")
+  common_ekf = File(f"{rednose_dir}/rednose/helpers/common_ekf.cc")
+
+  gen_script_deps = templates + [sympy_helpers, ekf_sym]
+  filter_lib_deps = [common_ekf]
+
+  env.AddMethod(CompileFilterMethod(gen_script_deps, filter_lib_deps), "RednoseCompileFilter")
+
+
+def exists(env):
+  return True
