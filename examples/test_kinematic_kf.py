2cf7434271debee5e6fceb812bd8bd64dd9bd889
Uku Loskit <ukuloskit@gmail.com>
2024-07-09 22:33:40 +0300

    Augment PYTHONPATH for pytest via pyproject.toml

--- examples/test_kinematic_kf.py
+++ examples/test_kinematic_kf.py
@@ -2,8 +2,6 @@ import pytest
 import os
 import numpy as np
 import unittest
-import sys
-sys.path.append(os.path.dirname(__file__))
 
 from kinematic_kf import KinematicKalman, ObservationKind, States  # pylint: disable=import-error
 
