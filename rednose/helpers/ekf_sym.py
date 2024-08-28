90c891fe8972e6e205c69f894c688cc12ef5ecb6
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-08-28 11:32:58 -0700

    fix "ResourceWarning: unclosed..."

--- rednose/helpers/ekf_sym.py
+++ rednose/helpers/ekf_sym.py
@@ -210,8 +210,10 @@ def gen_code(folder, name, f_sym, dt_sym, x_sym, obs_eqs, dim_x, dim_err, eskf_p
   if not os.path.exists(folder):
     os.mkdir(folder)
 
-  open(os.path.join(folder, f"{name}.h"), 'w', encoding='utf-8').write(header)  # header is used for ffi import
-  open(os.path.join(folder, f"{name}.cpp"), 'w', encoding='utf-8').write(code)
+  with open(os.path.join(folder, f"{name}.h"), 'w', encoding='utf-8') as f:
+    f.write(header)  # header is used for ffi import
+  with open(os.path.join(folder, f"{name}.cpp"), 'w', encoding='utf-8') as f:
+    f.write(code)
 
 
 class EKF_sym():
