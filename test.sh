483fc07bbec40cc06fc706806afd4bad9e0bbcce
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-07-11 12:48:13 -0700

    Bootstrap uv in test runner

--- test.sh
+++ test.sh
@@ -5,6 +5,10 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd "$DIR"
 
 # *** env setup ***
+if ! command -v uv >/dev/null; then
+  curl -LsSf https://astral.sh/uv/install.sh | sh
+  export PATH="$HOME/.local/bin:$PATH"
+fi
 uv sync --extra dev --locked
 
 # *** build ***
